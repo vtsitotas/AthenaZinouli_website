f6c7ca1ef27bf58061ae5fafa42f383271185dc4
vtsitotas <115449004+vtsitotas@users.noreply.github.com>
2026-01-27 11:15:01 +0200

    Add files via upload

---
+++ μαμαιστοσελιδα/script.js
@@ -0,0 +1,23 @@
+document.getElementById('sidebarToggle').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('active');
+    document.getElementById('overlay').classList.toggle('active');
+});
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const sections = document.querySelectorAll(".fade-in");
+
+        const observer = new IntersectionObserver(
+            (entries, observer) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add("visible");
+                        observer.unobserve(entry.target);
+                    }
+                });
+            },
+            { threshold: 0.2 }
+        );
+
+        sections.forEach(section => observer.observe(section));
+    });
+
